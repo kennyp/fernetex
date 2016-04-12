6635d3c7745cc878fdc8df9c2977ef30f5498dcf
Kenny Parnell <k.parnell@gmail.com>
2016-04-11 20:53:26 -0400

    Add fernet.sign mix task

---
+++ lib/mix/tasks/fernet_sign.ex
@@ -0,0 +1,25 @@
+defmodule Mix.Tasks.Fernet.Sign do
+  @moduledoc """
+  Sign STDIN using the given key
+
+      mix fernet.sign key
+  """
+
+  use Mix.Task
+
+  @shortdoc "Sign STDIN using fernet"
+
+  def run([key]) do
+    {:ok, _} = Application.ensure_all_started(:timex)
+
+    :stdio
+    |> IO.read(:all)
+    |> Fernet.generate(key: key)
+    |> elem(2)
+    |> Mix.shell.info
+  end
+
+  def run(_args) do
+    Mix.raise "usage: mix fernet.sign key"
+  end
+end
