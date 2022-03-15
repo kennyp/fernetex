f56221d9c091501443fcc6c3e15fd84f86f5fde6
Luca Corti <luca@sibill.it>
2022-03-15 20:40:24 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -4,14 +4,19 @@ defmodule Fernet.Mixfile do
   @version "0.4.0"
 
   def project do
-    [app: :fernetex,
-     version: @version,
-     elixir: "~> 1.10",
-     start_permanent: Mix.env == :prod,
-     docs: [source_ref: "v#{@version}", main: "Fernet",
-            source_url: "https://github.com/kennyp/fernetex"],
-     package: package(),
-     deps: deps()]
+    [
+      app: :fernetex,
+      version: @version,
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      docs: [
+        source_ref: "v#{@version}",
+        main: "Fernet",
+        source_url: "https://github.com/kennyp/fernetex"
+      ],
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
@@ -20,7 +25,8 @@ defmodule Fernet.Mixfile do
 
   defp package do
     [
-      description: "Elixir implementation of Fernet library based on https://github.com/fernet/spec",
+      description:
+        "Elixir implementation of Fernet library based on https://github.com/fernet/spec",
       files: ["lib", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["Kenny Parnell"],
       licenses: ["MIT"],
@@ -30,15 +36,15 @@ defmodule Fernet.Mixfile do
 
   defp deps do
     [
-     {:timex, "~> 3.1"},
+      {:timex, "~> 3.1"},
 
-     # Dev & Test dependencies
-     {:credo, "~> 1.5.6", only: [:dev, :test], runtime: false},
-     {:dialyxir, "~> 1.1.0", only: :dev},
-     {:earmark, "~> 1.2", only: :dev},
-     {:ex_doc, "~> 0.15", only: :dev},
-     {:poison , "~> 4.0.1", only: [:dev, :test]},
-     {:propcheck, "~> 1.0", only: :test}
+      # Dev & Test dependencies
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev},
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.15", only: :dev},
+      {:poison, "~> 5.0", only: [:dev, :test]},
+      {:propcheck, "~> 1.0", only: :test}
     ]
   end
 end
